a86998816e1bb51cdc7ef8234751179f88955411
inokawa <inokawa@users.noreply.github.com>
2025-03-02 11:57:54 +0000

    deploy: 11e25d019dba52a22850ce26c39a3779e463a368

--- assets/preview-CO7SZcdt.js
+++
@@ -1,1 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},layout:"fullscreen",options:{storySort:{order:["basics",["plain","custom"],"advanced"]}}};export{o as parameters};
---
+++ assets/preview-KIa_pAFc.js
@@ -0,0 +1,1 @@
+const e={actions:{argTypesRegex:"^on[A-Z].*"},layout:"fullscreen",options:{storySort:{order:["basics",["plain","structured"],"advanced"]}}};export{e as parameters};
