365d968e3ebf4ade831202dd21e78f5a12c1bef7
inokawa <inokawa@users.noreply.github.com>
2025-04-17 03:50:39 +0000

    deploy: a272a57d4de7a511ed9e32ce046c12fcb66c1a58

--- assets/plain-SFQgg530.js
+++ assets/plain-SFQgg530.js
@@ -1,2 +1,2 @@
-import{N as o}from"./editable-BiRxwCT1.js";const a=e=>e.reduce((t,r,p)=>(p!==0&&(t+=`
+import{N as o}from"./editable-CpBlIBvG.js";const a=e=>e.reduce((t,r,p)=>(p!==0&&(t+=`
 `),t+r.reduce((i,n)=>i+(n.type===o?n.text:""),"")),""),u=({multiline:e}={})=>({single:!e,js:a,void:()=>{},copy:(t,r)=>{t.setData("text/plain",a(r))},paste:t=>t.getData("text/plain")});export{u as p};
