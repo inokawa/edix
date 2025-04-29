25d644159373fff327cde933545fed32c46001ec
inokawa <inokawa@users.noreply.github.com>
2025-04-29 03:37:08 +0000

    deploy: 646f4d496abb939b6bf1a144edd09e3ea11d250b

--- assets/plain-DcbleqNQ.js
+++
@@ -1,1 +0,0 @@
-import{d as e,I as n}from"./editable-B8OCbUN9.js";const p=({multiline:s}={})=>({single:!s,js:e,void:()=>{},copy:(t,a)=>{t.setData("text/plain",e(a))},paste:(t,a)=>{a(n,t.getData("text/plain"))}});export{p};
