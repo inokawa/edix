6c7f9a963caa377079cf75203c2328388bad7bed
inokawa <inokawa@users.noreply.github.com>
2025-04-29 09:42:25 +0000

    deploy: 489ceee2269f8ec0418897c0c30525764db88f2a

---
+++ assets/plain-Do-LXX0w.js
@@ -0,0 +1,1 @@
+import{d as e,I as n}from"./editable-B7UdEcen.js";const p=({multiline:s}={})=>({single:!s,js:e,void:()=>{},copy:(t,a)=>{t.setData("text/plain",e(a))},paste:(t,a)=>{a(n,t.getData("text/plain"))}});export{p};
