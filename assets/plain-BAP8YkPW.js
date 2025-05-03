15a0b138b071da58d06d9ad5cc73efce5f077395
inokawa <inokawa@users.noreply.github.com>
2025-05-03 05:09:46 +0000

    deploy: 4033a48bd59495af109f97259b65fe7e0a4a64e2

--- assets/plain-BAP8YkPW.js
+++
@@ -1,1 +0,0 @@
-import{d as e,I as n}from"./editable-ZK5qw91P.js";const p=({multiline:s}={})=>({single:!s,js:e,void:()=>{},copy:(t,a)=>{t.setData("text/plain",e(a))},paste:(t,a)=>{a(n,t.getData("text/plain"))}});export{p};
