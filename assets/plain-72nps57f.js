a4dd0faad6ec2cfe6c200dee400b6d1354c7cadb
inokawa <inokawa@users.noreply.github.com>
2025-05-25 14:10:25 +0000

    deploy: f8a4d60ccfa662fa5df3c2e18e4fcc08fc605f91

--- assets/plain-72nps57f.js
+++
@@ -1,1 +0,0 @@
-import{d as a,s}from"./editable-Dw45K6-A.js";const r=({multiline:e}={})=>({single:!e,js:a,void:()=>{},copy:(t,o)=>{t.setData("text/plain",a(o))},paste:t=>s(t.getData("text/plain"))});export{r as p};
