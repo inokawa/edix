2c682eb04061606b1cedf85047058e81baca4058
inokawa <inokawa@users.noreply.github.com>
2025-06-01 03:43:12 +0000

    deploy: ff5898994c7a759362b38fd74b509941d0e30d15

---
+++ assets/plain-BAT3J7t2.js
@@ -0,0 +1,1 @@
+import{d as a,s}from"./editable-WhRiBgbC.js";const r=({multiline:e}={})=>({single:!e,js:a,void:()=>{},copy:(t,o)=>{t.setData("text/plain",a(o))},paste:t=>s(t.getData("text/plain"))});export{r as p};
