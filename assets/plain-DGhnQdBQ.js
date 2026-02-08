561a3d37ccf5e9c458bd9de85539cd5b0e792b0a
inokawa <inokawa@users.noreply.github.com>
2026-02-08 05:41:47 +0000

    deploy: b1a5bdeee3f5fe8b6b7af681429e16b5d153a925

---
+++ assets/plain-DGhnQdBQ.js
@@ -0,0 +1,1 @@
+import{c as e,s as n,d as c}from"./editor-Wp6T9U_x.js";const i=({text:r,onChange:o,...t})=>e({...t,doc:n(r),onChange:a=>{o(c(a))}});export{i as c};
--- assets/plain-DmV9EMBu.js
+++
@@ -1,1 +0,0 @@
-import{c as e,s as n,d as c}from"./editor-CJ9tJRbg.js";const i=({text:r,onChange:o,...t})=>e({...t,doc:n(r),onChange:a=>{o(c(a))}});export{i as c};
