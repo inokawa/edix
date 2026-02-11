2611eeadeeda9794e23dd3e8658744b4a06492f1
inokawa <inokawa@users.noreply.github.com>
2026-02-11 03:37:43 +0000

    deploy: 08068597a22c97150dd90bc0c878087f3601ef5c

--- assets/plain-B9oBfIM8.js
+++
@@ -1,1 +0,0 @@
-import{c as t,s as e,a as s,d as c}from"./singleline-BjimAA-J.js";const d=({text:n,singleline:o,onChange:r,...a})=>t({...a,doc:e(n),plugins:o?[s()]:void 0,onChange:i=>{r(c(i))}});export{d as c};
