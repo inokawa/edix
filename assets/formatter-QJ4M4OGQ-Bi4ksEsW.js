78bae746fd313c1302281b20712622721785043c
inokawa <inokawa@users.noreply.github.com>
2026-02-26 13:18:25 +0000

    deploy: c53d29702585850486a8d784188053a4b98acc03

---
+++ assets/formatter-QJ4M4OGQ-Bi4ksEsW.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-BGp3VvY-.js";import{I as t}from"./iframe-Dt85U81A.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-Cs0vWXl0.js";import"./index-B43ePn49.js";import"./index-BDzQ6Imv.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
---
+++ assets/index-BDzQ6Imv.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-Dt85U81A.js";import{r as o}from"./index-B43ePn49.js";var t=o();const m=r(t);export{m as R,t as r};
