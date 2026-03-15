391db951404bd2f5eaa1deda921384e44d015217
inokawa <inokawa@users.noreply.github.com>
2026-03-15 11:38:32 +0000

    deploy: 1d3f2b4764d32ae7009f864440654f33b18f8297

--- assets/formatter-EIJCOSYU-C-R1-r0V.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-LL677BLK-BMu4Ugjh.js";import{I as t}from"./iframe-Ba7SLKuu.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-BOJfXB6s.js";import"./index-B8NTHj_J.js";import"./index-DVOtCB1m.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
