e0de10504f80f988931cee80423a4bfa0a8493d3
inokawa <inokawa@users.noreply.github.com>
2026-07-27 08:52:16 +0000

    deploy: 9ab8adc7fc340ba89b89d40fd9dcc9a47358302a

--- plugins/scrollToSelection.d.ts
+++ plugins/scrollToSelection.d.ts
@@ -1,12 +1,5 @@
 import { Editor } from '../editor.js';
-export interface ScrollToSelectionOptions {
-    /**
-     * Scroll smoothly instead of jumping.
-     * @default false
-     */
-    smooth?: boolean;
-}
 /**
  * A plugin to scroll to the selection on document change.
  */
-export declare const scrollToSelectionPlugin: (editor: Editor, { smooth }?: ScrollToSelectionOptions) => void;
+export declare const scrollToSelectionPlugin: (editor: Editor) => void;
