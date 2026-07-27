d4825f3b7893f1b76e0a9cf5d354ebea4e17f3f8
inokawa <inokawa@users.noreply.github.com>
2026-07-27 08:43:38 +0000

    deploy: 7c508f2c32131e9588a74862389536770c8bc3d9

--- plugins/index.d.ts
+++ plugins/index.d.ts
@@ -2,5 +2,6 @@ export * from './transfer/index.js';
 export { debugPlugin } from './debug.js';
 export { keymapPlugin } from './keymap.js';
 export { Undo, Redo, Undoable, Redoable } from './history.js';
+export { scrollToSelectionPlugin } from './scrollToSelection.js';
 export { selectionRectPlugin } from './selectionRect.js';
 export { singlelinePlugin } from './singleline.js';
