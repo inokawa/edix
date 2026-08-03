96827c8dd456a9aaaf7a30be8abe736376cd162b
inokawa <inokawa@users.noreply.github.com>
2026-08-03 15:26:01 +0000

    deploy: 0718ad1fd8cbb6c9332cb46ab468fabc956971ac

--- plugins/transfer/internalTransfer.d.ts
+++ plugins/transfer/internalTransfer.d.ts
@@ -1,5 +1,10 @@
 import { Editor } from '../../editor.js';
 /**
  * A plugin to handle copying / pasting between editor instances
+ *
+ * @param options.mime A MIME type to store the copied fragment in clipboard. Give an app specific one if the schema is not shared with other editate based apps.
+ * @defaultValue `"application/x-editate-editor"`
  */
-export declare function internalTransferPlugin(editor: Editor): void;
+export declare function internalTransferPlugin(editor: Editor, options?: {
+    mime?: string;
+}): void;
